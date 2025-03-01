875dd6cbd0035335ce9790862ecf159d427c6435
Zephyr <126724249+Unknownlodfc@users.noreply.github.com>
2025-03-01 17:07:35 +0100

    Create Example.lua

---
+++ NotificationSystem/Example.lua
@@ -0,0 +1,13 @@
+--Notification Example, is the title
+
+--This is a notification message, is the description/info
+
+--5, is the duration
+
+--RightTop (Right Top of the Screen)
+--LeftTop (Left Top of thd Screen)
+--RightBottom (Right Bottom of the Screen)
+--LeftBottom (Left Bottom of the Screen)
+
+--255, 255, 255 is the title color. (you can find these colors at: https://htmlcolorcodes.com)
+createNotification("Notification Example", "This is a notification message.", 5, LeftBottom, 255, 255, 255)
