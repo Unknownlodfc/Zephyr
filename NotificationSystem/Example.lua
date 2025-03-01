4b6f7303f9cde1c2c47208f0e084213169e06ce9
Zephyr <126724249+Unknownlodfc@users.noreply.github.com>
2025-03-01 17:17:54 +0100

    Update Example.lua

--- NotificationSystem/Example.lua
+++ NotificationSystem/Example.lua
@@ -10,6 +10,8 @@
 --LeftBottom (Left Bottom of the Screen)
 
 --255, 255, 255 is the title color. (you can find these colors at: https://htmlcolorcodes.com)
+
+--if it doesn't work go to this website: https://github.com/Unknownlodfc/Zephyr/blob/main/NotificationSystem/If%20it%20doesn't%20work.lua
 local createNotification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Unknownlodfc/Zephyr/refs/heads/main/NotificationSystem/Notification.lua"))()
 
 createNotification("Right Top", "This is a notification message.", 5, RightTop, 255, 255, 255)
